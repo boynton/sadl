7c9088dec185717c56243fe851dfcd9bc8e50f30
Lee R. Boynton <lee@boynton.com>
2019-02-11 18:01:02 -0800

    More fixes. Nested struct java generation, Timestamp
    Flag to use java.time.Instant instead of generated Timestamp class. Even then it uses TImestamp.InstantSerializer to be RFC 3339 compliant. Fixed UUID aliasing.

---
+++ cmd/sadl2java/createTimestamp.go
@@ -0,0 +1,80 @@
+package main
+
+func (gen *PojoGenerator) createTimestamp() {
+	gen.createJavaFile("Timestamp")
+	if gen.err != nil {
+		return
+	}
+	gen.emit(javaTimestamp)
+	gen.writer.Flush()
+	gen.file.Close()
+}
+
+var javaTimestamp = `
+import java.time.Instant;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+
+@JsonSerialize(using = Timestamp.Serializer.class)
+@JsonDeserialize(using = Timestamp.Deserializer.class)
+public class Timestamp {
+    private String repr;
+
+    public static Timestamp now() {
+        return new Timestamp(Instant.now());
+    }
+
+    public Timestamp(String repr) {
+        this.repr = repr;
+    }
+
+    public Timestamp(Instant instant) {
+        this.repr = instant.toString();
+    }
+    
+    public String toString() {
+        return repr;
+    }
+
+    public Instant asInstant() {
+        return Instant.parse(repr);
+    }
+
+    public static class Serializer extends JsonSerializer<Timestamp> {
+        @Override
+        public void serialize(Timestamp value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeString(value.repr);
+        }
+    }
+    public static class Deserializer extends JsonDeserializer<Timestamp> {
+        @Override
+        public Timestamp deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            String s = jp.getText();
+            return new Timestamp(s);
+        }
+    }
+
+    public static class InstantSerializer extends JsonSerializer<Instant> {
+        @Override
+        public void serialize(Instant value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeString(value.toString());
+        }
+    }
+    public static class InstantDeserializer extends JsonDeserializer<Instant> {
+        @Override
+        public Instant deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            String s = jp.getText();
+            return Instant.parse(s);
+        }
+    }
+
+}
+`
